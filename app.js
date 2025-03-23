33bae1130c3f4d79801736545a33d68abf4b6113
Maya Widyasari <56695215+mayawid@users.noreply.github.com>
2025-03-23 08:02:22 -0700

    Putting version

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Cubing World!');
+    res.send('Hello Cubing World v1!');
 });
 
 const PORT = 3000;
